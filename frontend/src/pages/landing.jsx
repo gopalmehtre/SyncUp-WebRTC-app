41ee25b6ff058989f3e235941cb218986cceda3f
Gopal <gopalmehtre10@gmail.com>
2025-09-21 04:57:18 +0530

    added landing page

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import "../App.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+export default function LandingPage() {
+  return (
+    <div className='landingPageContainer'>
+        <nav>
+            <div className='navHeader'>
+                <h2><i class="fa-solid fa-globe"></i>SyncUP</h2>
+            </div>
+
+            <div className='navlist'>
+                <p>Join as Guest</p>
+                <p>Register</p>
+                <div role='button'>
+                    <p>Login</p>
+                </div>
+                
+
+            </div>
+        </nav>
+
+        <div className='landingMainContainer'>
+            <div>
+                <h1><span style={{color: '#9999ff'}} >Connect</span> with your loved Ones!</h1>
+                <p>Cover distance with SyncUP!</p>
+                <div role='button' >
+                    <Link to={"/auth"}>Get Started</Link>
+                </div>
+            </div>
+            <div>
+                    <img src="/mobile.png" alt="" />
+                </div>
+        </div>
+
+    </div>
+  )
+}
