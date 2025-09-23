f3ccd927c0f7a01365d6cbb37028497fa5ab06d0
Gopal <gopalmehtre10@gmail.com>
2025-09-24 03:12:12 +0530

    implemented-authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@
 import './App.css';
 import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
 import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import {VideoMeetComponent} from './pages/videoMeet';
+
 
 function App() {
   
@@ -11,9 +15,17 @@ function App() {
       <div className='App'>
 
         <Router>
+
+          <AuthProvider>
           <Routes>
             <Route path = '/' element = {<LandingPage/>} ></Route>
+
+            <Route path='/auth' element={<Authentication/>}></Route>
+
+            <Route path='/:url' element={<VideoMeetComponent/>} ></Route>
           </Routes>
+          </AuthProvider>
+
         </Router>
 
       </div>
